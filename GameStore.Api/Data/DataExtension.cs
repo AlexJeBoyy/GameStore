83847042d936ab4d2206bfc43a70989bf998d0eb
AlexJeBoyy <alexstrikwerda@hotmail.com>
2025-08-24 23:30:59 +0200

    Add genre endpoints and DTO to API
    Introduced GenreDto, GenreEndpoints, and GenreMapping to support genre retrieval via a new /genres endpoint. Updated Program.cs to register genre endpoints and renamed the database migration method to MigrateDbAsync for consistency.

--- GameStore.Api/Data/DataExtension.cs
+++ GameStore.Api/Data/DataExtension.cs
@@ -6,7 +6,7 @@ namespace GameStore.Api.Data;
 
 public static class DataExtension
 {
-    public static async Task MigrateDb(this WebApplication app)
+    public static async Task MigrateDbAsync(this WebApplication app)
     {
         using var scope = app.Services.CreateScope();
         var dbContext = scope.ServiceProvider.GetRequiredService<GameStoreContext>();
--- GameStore.Api/obj/Debug/net9.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore.Api/obj/Debug/net9.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f291dee44c71898ec5972da6b847b3b300a6db0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70428627cac29a2890f90893ae33a5cde919213f")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
