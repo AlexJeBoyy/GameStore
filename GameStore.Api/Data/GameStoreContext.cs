0f291dee44c71898ec5972da6b847b3b300a6db0
AlexJeBoyy <alexstrikwerda@hotmail.com>
2025-08-24 22:04:41 +0200

    Seed genres and enable database migrations
    Added seeding of initial genres to the database via EF Core migrations. Introduced DataExtension for automatic migration on startup, updated CreateGameDto to use GenreId, and refactored game creation to persist games in the database. Adjusted logging for EF Core database commands.

--- GameStore.Api/Data/GameStoreContext.cs
+++ GameStore.Api/Data/GameStoreContext.cs
@@ -10,4 +10,19 @@ public class GameStoreContext(DbContextOptions<GameStoreContext> options) : DbCo
 
     public DbSet<Genre> Genres => Set<Genre>();
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Genre>().HasData(
+            new { Id = 1, Name = "Action" },
+            new { Id = 2, Name = "Adventure" },
+            new { Id = 3, Name = "RPG" },
+            new { Id = 4, Name = "Strategy" },
+            new { Id = 5, Name = "Simulation" },
+            new { Id = 6, Name = "Sports" },
+            new { Id = 7, Name = "Puzzle" },
+            new { Id = 8, Name = "Horror" },
+            new { Id = 9, Name = "Shooter" },
+            new { Id = 10, Name = "Platformer" }
+        );
+    }
 }
--- GameStore.Api/obj/Debug/net9.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore.Api/obj/Debug/net9.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37eaec60ce097b74249f59bc734764652c8fca1d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+383903a5319cdb0f346dc5a6e91e49925b7a6151")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
